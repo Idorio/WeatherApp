c1f792f1ed5e40b4d8428ea08bbe8509db5e8070
Idorio <insaider9218@gmail.com>
2023-02-27 12:41:07 +0300

    Preparation

---
+++ app/src/main/java/com/example/weatherapp/SettingsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SettingsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+}
