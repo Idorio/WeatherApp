3e47a1724cdb0e387085a2a242d8b4788ec373c5
Idorio <insaider9218@gmail.com>
2023-03-12 09:54:20 +0300

    hilt

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction().replace(R.id.activity_container,WeatherFragment())
             .commit()
+
     }
 
 }
